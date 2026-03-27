24ac5dd63b1299d299cf071b1c0f421bed146972
huychuoi-hub <nth522011@gmail.com>
2026-03-27 14:47:38 +0700

    Update chuoirayhub.lua

--- chuoirayhub.lua
+++ chuoirayhub.lua
@@ -1,1 +1,9 @@
+local currentId = game.PlaceId
 
+if currentId == 994732206 then 
+    print("LOADING")
+    loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/huychuoi-hub/kaitun-beta/refs/heads/main/chuoirayhub.lua"))()"))()
+
+else
+    print(":....... " .. currentId)
+end
