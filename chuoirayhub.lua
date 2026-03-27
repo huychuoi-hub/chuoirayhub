552eb7518da5347a12338eb76260a99030eb41f3
huychuoi-hub <nth522011@gmail.com>
2026-03-27 15:12:52 +0700

    Update chuoirayhub.lua

--- chuoirayhub.lua
+++ chuoirayhub.lua
@@ -1,12 +1,19 @@
-local currentId = game.PlaceId
+local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 
-if currentId == 9503696692 then 
-    print("Đang chạy script Blox Fruits...")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/huychuoi-hub/SILENT-ASSASSIN/refs/heads/main/chuoirayhub.lua"))()
-    
-elseif currentId == 994732206 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/huychuoi-hub/kaitun-beta/refs/heads/main/chuoirayhub.lua"))()
+local MyScripts = {
+    [994732206] = "https://raw.githubusercontent.com/huychuoi-hub/kaitun-beta/refs/heads/main/chuoirayhub.lua"
+    [9503696692] = "https://raw.githubusercontent.com/huychuoi-hub/SILENT-ASSASSIN/refs/heads/main/chuoirayhub.lua"
+}
 
+local CurrentScript = MyScripts[game.PlaceId] or MyScripts[UniverseID]
+
+if CurrentScript then
+    print("Chuoi Ray Hub: Game Detected!")
+    loadstring(game:HttpGet(CurrentScript))()
 else
-    print("Game này chưa được hỗ trợ ID: " .. currentId)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Chuoi Ray Hub",
+        Text = "Game chưa hỗ trợ! ID: " .. game.PlaceId,
+        Duration = 10
+    })
 end
